32893867d9c1e6c1434c3ed0f8fb90376a663a0b
Finnegan Adam <finnegan.adam@code.berlin>
2026-04-30 10:31:13 +0200

    Add validation to book model

--- models/Buch.js
+++ models/Buch.js
@@ -3,16 +3,17 @@ var mongoose = require('mongoose')
 var buchSchema = new mongoose.Schema({
   titel: {
     type: String,
-    required: true,
+    required: [true, 'Titel ist erforderlich'],
     trim: true,
     maxlength: 200
   },
   autor: {
     type: String,
-    required: true
+    required: [true, 'Autor ist erforderlich']
   },
   seiten: {
-    type: Number
+    type: Number,
+    min: [1, 'Minimum 1 page']
   },
   gelesen: {
     type: Boolean,
